a80f772b7134111de8baa20aa67d57e05075d8cd
BrendoWise2 <brendo.oliveira@aluno.unincor.edu.br>
2025-03-15 16:29:30 -0300

    5.1V

--- celulaSearch.js
+++ celulaSearch.js
@@ -126,7 +126,7 @@ bntExpandir.addEventListener('click', () => {
     console.log('Botão de expandir clicado');
     let linhas = document.querySelectorAll('.linha');
     linha.forEach((linha => {
-        linha.classList.add('linha-active')
+        linha.classList.add('active')
         console.log('w');
     }));
 
